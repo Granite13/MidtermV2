f3e50c075bd5204ae0fce45172ef9e232c9daeef
Granite13 <whyaguilar@bscs.usjr.edu.ph>
2016-02-23 16:57:32 +0800

    Creating a Class for Menu

---
+++ app/src/main/java/com/aguilar/midtermv2/Menu.java
@@ -0,0 +1,24 @@
+package com.aguilar.midtermv2;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * Created by whyaguilar on 2/23/16.
+ */
+public class Menu extends ListActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
+}
