dfd5a7698f0a4ee64c8449e6bb7f9884811627c8
Granite13 <whyaguilar@bscs.usjr.edu.ph>
2016-02-23 17:35:40 +0800

    Creating a Class for Menu Setting up values for List

--- app/src/main/java/com/aguilar/midtermv2/Menu.java
+++ app/src/main/java/com/aguilar/midtermv2/Menu.java
@@ -1,24 +1,63 @@
 package com.aguilar.midtermv2;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by whyaguilar on 2/23/16.
  */
 public class Menu extends ListActivity {
 
+    //temporary for listActivity
 
+    private static String url = "http://172.17.3.51:5856/api/books/";
+
+    private static final String TAG_ID = "id";
+    private static final String TAG_TITLE = "name";
+    private static final String TAG_GENRE = "genre";
+    private static final String TAG_AUTHOR = "author";
+    private static final String TAG_ISREAD = "status";
+
+
+    JSONArray contacts = null;
+
+    // Hashmap for ListView
+    ArrayList<HashMap<String, String>> booklist;
+
+
+    String booklisted[] = {"Name of Book 1","Name of Book 2","Name of Book 3","Name of Book 4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(Menu.this,android.R.layout.simple_list_item_1,booklisted));
+        booklist = new ArrayList<HashMap<String, String>>();
+        ListView lv = getListView();
     }
+
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
+
+        try{
+            Intent ourIntent = new Intent();
+            startActivity(ourIntent);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
     }
 }
